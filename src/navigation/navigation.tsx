c5c9ecdd58c3e3800a6d614b189760378ab86d04
Dakito24 <dakstir24@gmail.com>
2024-02-13 15:37:21 -0600

    Changed navigation

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,16 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import StopWatch from '../screens/StopWatch/stopwatch'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StopWatch from "../screens/StopWatch/stopwatch";
+import Clock from "../screens/Clock/clock";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function Router() {
-    return (
-        <BrowserRouter>
+  return (
+    <BrowserRouter>
       <Routes>
-      <Route path="/">
-        <Route path="stopwatch" element={<StopWatch />} />
-      </Route>
+        <Route path="/" element={<Clock />} />
+        <Route path="/stopwatch" element={<StopWatch />} />
       </Routes>
     </BrowserRouter>
-    )
+  );
 }
